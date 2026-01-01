9dfc9ed523b39961d816e1ea3157ee1c567fb2bd
Michael Gor <michaelgor@gmx.de>
2026-01-02 00:27:27 +0100

    cond, define a function  directly, basic error function

---
+++ deriv_new.scm
@@ -0,0 +1,34 @@
+(define (map f l)
+  (if (null? l)
+      '()
+      (cons (f (car l)) (map f (cdr l)))))
+
+(define (deriv a)
+  (cond ((not (pair? a))
+         (if (eq? a 'x) 1 0))
+        ((eq? (car a) '+)
+         (cons '+
+               (map deriv (cdr a))))
+        ((eq? (car a) '-)
+         (cons '-
+               (map deriv (cdr a))))
+        ((eq? (car a) '*)
+         (list '*
+                a
+                (cons '+
+                      (map (lambda (a) (list '/ (deriv a) a)) (cdr a)))))
+        ((eq? (car a) '/)
+         (list '-
+               (list '/
+                     (deriv (cadr a))
+                     (caddr a))
+               (list '/
+                     (cadr a)
+                     (list '*
+                           (caddr a)
+                           (caddr a)
+                           (deriv (caddr a))))))
+        (else
+         (error #f "No derivation method available"))))
+
+(deriv '(+ x 3))
